7450b363f2f8b2ed8fa4076fb04c97a97dfab255
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-11-03 16:14:15 -0400

    feat: demo finished

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import * as dotenv from 'dotenv'
 import morgan from 'morgan'
 import cors from 'cors'
+import path from 'path'
 import boxRouter from './router'
 
 dotenv.config()
@@ -13,16 +14,24 @@ app.use(morgan('dev'))
 app.use(cors({ origin: 'http://localhost:5173' }))
 
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/test"
-const PORT = process.env.PORT || 3000
+const PORT = Number(process.env.PORT) || 3000
 
 mongoose.connect(MONGO_URI)
 
-app.get('/api', (req, res) => {
+app.get('/api', (_req, res) => {
     res.send('Hello World!')
 })
 
 app.use('/api/boxes', boxRouter)
 
-app.listen(PORT, () => {
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "build")))
+    app.get("*", (_req, res) => {
+        res.sendFile(path.join(__dirname, "build/index.html"))
+    })
+}
+
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`)
 })
